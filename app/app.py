44347836f6c65d09e55542b978ae01c69e27a960
JesusRamirezGamarra <luciojesusramirezgamarra@gmail.com>
2024-12-11 22:46:59 -0500

    first commit

---
+++ app/app.py
@@ -0,0 +1,15 @@
+import os
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    # Muestra el ID del contenedor para verificar el balanceo de carga
+    container_id = os.uname()[1]
+    #return f"Hello, World! from container {container_id}"
+    return f"Hello world: Ramirez Gamarra Lucio Jesus! from container {container_id}"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080)
